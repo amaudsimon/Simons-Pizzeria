ea55f9050af831c4647ccdca60d5444f156a44cb
Amaud Simon <asimo29@wgu.edu>
2023-06-29 14:35:56 -0500

    PartService.java	17	Added the throws illegal argument exception clause to indicate the method may throw an exception if the validation fails

--- src/main/java/com/example/demo/service/PartService.java
+++ src/main/java/com/example/demo/service/PartService.java
@@ -14,7 +14,8 @@ import java.util.List;
 public interface PartService {
     public List<Part> findAll();
     public Part findById(int theId);
-    public void save (Part thePart);
+    public void save(Part thePart) throws IllegalArgumentException;
+
     public void deleteById(int theId);
 
     public List<Part> listAll(String keyword);
