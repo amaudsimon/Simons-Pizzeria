9cab217768b73f433cc9e5fc1452189e6d567e9a
Amaud Simon <asimo29@wgu.edu>
2023-06-30 09:33:46 -0500

    BootStrapData.java	45 & 50	Add company name to Outsourced parts

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,78 +1,66 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
-import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
-import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
-import com.example.demo.service.OutsourcedPartService;
-import com.example.demo.service.OutsourcedPartServiceImpl;
-import com.example.demo.service.ProductService;
-import com.example.demo.service.ProductServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 
-/**
- *
- *
- *
- *
- */
 @Component
 public class BootStrapData implements CommandLineRunner {
 
-    private final PartRepository partRepository;
+    private final InhousePartRepository inhousePartRepository;
+    private final OutsourcedPartRepository outsourcedPartRepository;
     private final ProductRepository productRepository;
 
-    private final OutsourcedPartRepository outsourcedPartRepository;
-
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
-        this.partRepository = partRepository;
+    public BootStrapData(InhousePartRepository inhousePartRepository, OutsourcedPartRepository outsourcedPartRepository, ProductRepository productRepository) {
+        this.inhousePartRepository = inhousePartRepository;
+        this.outsourcedPartRepository = outsourcedPartRepository;
         this.productRepository = productRepository;
-        this.outsourcedPartRepository=outsourcedPartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
-
-
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            System.out.println(part.getName()+" "+part.getCompanyName());
-        }
-        if (partRepository.count() == 0 && productRepository.count() == 0) {
-            Part cheese = new Part("Cheese", 1.00, 10);
+        if (inhousePartRepository.count() == 0 && outsourcedPartRepository.count() == 0 && productRepository.count() == 0) {
+            InhousePart cheese = new InhousePart("Cheese", 1.00, 10);
             cheese.setMinInv(2);
             cheese.setMaxInv(20);
-            Part beef = new Part("Beef", 2.50, 10);
+
+            InhousePart beef = new InhousePart("Beef", 2.50, 10);
             beef.setMinInv(2);
             beef.setMaxInv(20);
-            Part peperoni = new Part("Peperoni", .75, 10);
+
+            InhousePart peperoni = new InhousePart("Pepperoni", 0.75, 10);
             peperoni.setMinInv(2);
             peperoni.setMaxInv(20);
-            Part chicken = new Part("Chicken", 1.00, 10);
+
+            OutsourcedPart chicken = new OutsourcedPart("Chicken", 1.00, 10);
             chicken.setMinInv(2);
             chicken.setMaxInv(20);
-            Part mixVeggies = new Part("Veggies", .75, 10);
+            chicken.setCompanyName("Chicken Supplier");
+
+            OutsourcedPart mixVeggies = new OutsourcedPart("Mix Veggies", 0.75, 10);
             mixVeggies.setMinInv(2);
             mixVeggies.setMaxInv(20);
+            mixVeggies.setCompanyName("Veggie Supplier");
 
             Product cheesePizza = new Product("Cheese Pizza", 10.00, 5);
-            Product peperoniPizza = new Product("Peperoni Pizza", 10.00, 5);
+            Product peperoniPizza = new Product("Pepperoni Pizza", 10.00, 5);
             Product beefPizza = new Product("Beef Pizza", 10.00, 5);
             Product chickenPizza = new Product("Chicken Pizza", 10.00, 5);
             Product veggiePizza = new Product("Veggie Pizza", 8.00, 10);
 
-            partRepository.save(cheese);
-            partRepository.save(beef);
-            partRepository.save(peperoni);
-            partRepository.save(chicken);
-            partRepository.save(mixVeggies);
+            inhousePartRepository.save(cheese);
+            inhousePartRepository.save(beef);
+            inhousePartRepository.save(peperoni);
+
+            outsourcedPartRepository.save(chicken);
+            outsourcedPartRepository.save(mixVeggies);
 
             productRepository.save(cheesePizza);
             productRepository.save(peperoniPizza);
@@ -88,25 +76,13 @@ public class BootStrapData implements CommandLineRunner {
             peperoniPizza.getParts().add(peperoni);
             chicken.getProducts().add(chickenPizza);
             chickenPizza.getParts().add(chicken);
-            mixVeggies.getProducts().add(veggiePizza);
-            veggiePizza.getParts().add(mixVeggies);
+
+            System.out.println("Started in Bootstrap");
+            System.out.println("Number of Products: " + productRepository.count());
+            System.out.println(productRepository.findAll());
+            System.out.println("Number of Parts: " + (inhousePartRepository.count() + outsourcedPartRepository.count()));
+            System.out.println("In-house Parts: " + inhousePartRepository.findAll());
+            System.out.println("Outsourced Parts: " + outsourcedPartRepository.findAll());
         }
-
-
-
-
-
-
-
-
-
-
-
-        System.out.println("Started in Bootstrap");
-        System.out.println("Number of Products"+productRepository.count());
-        System.out.println(productRepository.findAll());
-        System.out.println("Number of Parts"+partRepository.count());
-        System.out.println(partRepository.findAll());
-
     }
 }
